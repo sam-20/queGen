a24bdf26abb9e2b9af8e69896ac3431d14771f53
Samuel Asare-Botwe <botwesamuel192@gmail.com>
2023-04-27 18:40:21 +0100

    update

--- src/pages/Questions/SQL Practice.js
+++ src/pages/Questions/SQL Practice.js
@@ -45,14 +45,78 @@ export const SQLP = [
       </>
     ),
   },
-  { que: ``, ans: `` },
-  { que: ``, ans: `` },
-  { que: ``, ans: `` },
-  { que: ``, ans: `` },
-  { que: ``, ans: `` },
-  { que: ``, ans: `` },
-  { que: ``, ans: `` },
-  { que: ``, ans: `` },
+  {
+    que: `sort data by a single column`,
+    ans: (
+      <>
+        <p>SELECT student_first_name from student</p>
+        <p>ORDER BY student_first_name</p>
+      </>
+    ),
+  },
+  {
+    que: `sort data by multiple columns`,
+    ans: (
+      <>
+        <p>
+          SELECT student_number, student_year, student_first_name FROM `student`
+        </p>
+        <p>ORDER BY student_year,student_first_name</p>
+      </>
+    ),
+  },
+  {
+    que: `sort data by multiple columns using different sort directions for each column`,
+    ans: (
+      <>
+        <p>
+          SELECT student_number, student_year, student_first_name FROM `student`
+        </p>
+        <p>ORDER BY student_year ASC,student_first_name DESC</p>
+      </>
+    ),
+  },
+  {
+    que: `filter data which satisfies a condition`,
+    ans: (
+      <>
+        <p>
+          SELECT student_number, student_year, student_first_name FROM `student`
+        </p>
+        <p>WHERE student_booking_payment_status = "F"</p>
+        <b>NB:</b>Conditional operators are =, !=, &lt;, {"<="} , &gt;,{">="}
+      </>
+    ),
+  },
+
+  {
+    que: `filter data whose condition falls inside a range. Eg. select students whose ids are between 20 and 30`,
+    ans: (
+      <>
+        <p>SELECT * FROM room</p>
+        <p>WHERE room_id BETWEEN 5 and 10</p>
+      </>
+    ),
+  },
+  {
+    que: `filter data which satisfies one or multiple conditions. Eg. retrieve students who are in 1st/4th year and paid their fees`,
+    ans: (
+      <>
+        <p>SELECT * FROM room WHERE</p>
+        <p>(hall_id = 1 OR hall_id = 4)</p>
+        <p>AND (room_beds_vacant {">"} 2)</p>
+      </>
+    ),
+  },
+  {
+    que: `filter data where you have to use a bunch of OR statements`,
+    ans: (
+      <>
+        <p>SELECT * FROM room WHERE</p>
+        <p>hall_id IN (1,3,4)</p>
+      </>
+    ),
+  },
   { que: ``, ans: `` },
   { que: ``, ans: `` },
   { que: ``, ans: `` },
