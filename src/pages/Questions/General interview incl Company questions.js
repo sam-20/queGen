3253225e166c311b687c5fb0278e8b4deb5dd1ba
Samuel Asare-Botwe <botwesamuel192@gmail.com>
2023-04-28 20:14:39 +0100

    update

---
+++ src/pages/Questions/General interview incl Company questions.js
@@ -0,0 +1,18 @@
+export const GI = [
+  {
+    que: `Why you decided to apply here and why should we select you?`,
+    ans: ``,
+  },
+  {
+    que: `Tell us about a project that you worked on and felt proud of it`,
+    ans: ``,
+  },
+  {
+    que: `What are you looking for in your next job/journey?`,
+    ans: ``,
+  },
+  {
+    que: `Wat is your current salary & what are your salary expectations?`,
+    ans: ``,
+  },
+];
