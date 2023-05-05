8836905e41a9797c6ce3f04da0cf7c2b33888ae0
Samuel Asare-Botwe <botwesamuel192@gmail.com>
2023-05-05 08:25:16 +0100

    update

--- src/pages/Questions/General interview incl Company questions.js
+++ src/pages/Questions/General interview incl Company questions.js
@@ -1,19 +1,56 @@
 export const GI = [
   {
+    que: `why do you want to work here?`,
+    ans: `I decided to apply here for this role at Leigh Day because i was moved 
+  by the organisation’s goal to support people who suffer 
+  human rights abuse and or discrimination.
+  I believe this aligns with my desire to support people through difficult times 
+  and this is an experience i have had coming
+  from a country where the standard of living is not as great as you'd expect it to be.
+  I believe i can contribute to this cause as a data analyst
+  by using my analytical skills to derive insights from data which can be used
+   to make good decisions that would benefit the community and the nation as a whole.
+  `,
+  },
+  {
     que: `Why have you decided to apply here and why should we select you?`,
-    ans: ``,
+    ans: `I decided to apply here for this role at Leigh Day because i was moved 
+    by the organisation’s goal to support people who suffer 
+    human rights abuse and or discrimination.
+    I believe this aligns with my desire to support people through difficult times 
+    and this is an experience i have had coming
+    from a country where the standard of living is not as great as you'd expect it to be.
+    I believe i can contribute to this cause as a data analyst
+    by using my analytical skills to derive insights from data which can be used
+     to make good decisions that would benefit the community and the nation as a whole.
+    `,
   },
   {
     que: `Why do you want to work as an analyst at our organisation?`,
-    ans: null,
+    ans: `I decided to apply here for this role at Leigh Day because i was moved 
+    by the organisation’s goal to support people who suffer 
+    human rights abuse and or discrimination.
+    I believe this aligns with my desire to support people through difficult times 
+    and this is an experience i have had coming
+    from a country where the standard of living is not as great as you'd expect it to be.
+    I believe i can contribute to this cause as a data analyst
+    by using my analytical skills to derive insights from data which can be used
+     to make good decisions that would benefit the community and the nation as a whole.
+    `,
   },
   {
     que: `What qualities do you have that make you a good analyst?`,
-    ans: null,
+    ans: `I believe an analyst should possess the technical skills to be able
+    to draw insights from data to make good decisions. And from my past experience,
+     I have developed strong analytical and problem solving skills 
+     from the use of big data and analytical tools to develop a business solution.
+     Also, a good analyst should possess strong communication and teamwork skills.
+     These are qulaities i have gained from working as a tutor and analyst in the past
+    `,
   },
   {
     que: `Tell us about a project that you worked on and felt proud of it`,
-    ans: ``,
+    ans: `first time tutoring people in web dev and helping them secure jobs`,
   },
   {
     que: `What are you looking for in your next job/journey?`,
@@ -28,22 +65,111 @@ export const GI = [
     ans: null,
   },
   { que: `What change would you bring to this organisation?`, ans: null },
-  { que: `What makes you unique from others?`, ans: null },
+  {
+    que: `What makes you unique from others?`,
+    ans: `I have strong technical skills from the field data analytics 
+  and software programming. Combining these with the excellent communication and
+  teamwork skills I have acquired from past experience 
+  gives me a unique trait of establishing good working relationships while delivering solutions.`,
+  },
   { que: `why should we hire you?`, ans: null },
   {
-    que: `Do you have any technical skills? Can you list your database skills or business intelligence skills?`,
-    ans: null,
+    que: `Do you have any technical skills? In other words, what are your technical skills?`,
+    ans: `I have analytical skills from the use of tools 
+    like excel, sql and power BI which I gained from my postgraduate studies 
+    and previous work experience as a junior data analyst and
+    I also have programming skills from the use of technologies like js,php and java 
+    which I gained from my undergraduate studies and previous work experience 
+    as an applications developer`,
   },
   { que: `What is xyz as a company?`, ans: null },
   { que: `What are the goals of the company?`, ans: null },
   { que: `What are the core values of the company?`, ans: null },
-  { que: `why do you want to work here?`, ans: null },
-  { que: `Tell me about yourself`, ans: null },
-  { que: `what is your greatest strength and weakness?`, ans: null },
-  { que: `what would your co-workers say about you?`, ans: null },
-  { que: `what are your salary expectations?`, ans: null },
-  { que: `what do you like to do outside of work?`, ans: null },
-  { que: `where do you see yourself in 5 years?`, ans: null },
+  {
+    que: `Tell me about yourself`,
+    ans: `My name is Samuel and I am from Ghana. I arrived in the UK in 2021 to
+    study a postgraduate course in Data Science and Analytics which I completed last year.
+    I am very passionate about exploring new technologies so 
+    back in Ghana, I worked as an applications developer for close to 3 years before moving 
+    into the UK here I worked primarily as a data analyst for close to a year.
+    I often appear to be very calm and I value the relationships I have with friends
+    and family a lot because I believe its through these relationships and
+    working together that we can inspire and learn from others.
+  `,
+  },
+  {
+    que: `what is your greatest strength and weakness?`,
+    ans: `I believe my greates strength would be my determination to succeed. For someone
+  who is passionate about technology, I often come across some concepts which appear to 
+  be complex to grasp or understand. But these are the challenges which motivate me
+  to know more because I am always willing to overcome the hurdle in order to progress
+  onto the next stage of my life. Another strength I'd say I possess is how well i handle
+  my relationship with the people around me because I am very humble and respectful. 
+  My weakness would probably be my cooking. It's terrible.
+  `,
+  },
+  {
+    que: `what would your co-workers say about you?`,
+    ans: `Well they'd probably tell you I am always eager to learn. Theyd also say I am very
+  humble and respectful because you also need these qualities if you want to learn
+  `,
+  },
+  {
+    que: `what are your salary expectations?`,
+    ans: `
+  I would say I am pleased with the salary range given in the job description. Although 
+  i'd hope to see an increase in future.
+  `,
+  },
+  {
+    que: `what do you like to do outside of work?`,
+    ans: `
+  I love music a lot. And anime as well. Then also i love to spend a lot of time with my
+  family on the phone since they're not here in the UK
+  `,
+  },
+  {
+    que: `where do you see yourself in 5 years?`,
+    ans: `My ambition is to work as a Data Analyst or Business Intelligence Analyst 
+    in any industry where my expertise is required. I am very passionate about use
+      data to solve problems and increase business values and in the next 1-2 years, I 
+      want to acquire new knowledge, skills and work experience as well as
+       improving my existing ones. I am looking forward to becoming the Senior 
+       Data Analyst of a large organisation in the next 3-5 years 
+       so that I can use my knowledge and wealth
+         of experience to solve very challenging issues and have a significant influence on 
+         change that improves everyone's standard of living.
+    `,
+  },
+  {
+    que: `what are you goals or ambitions? In other words what are your career aspirations for the future`,
+    ans: `My ambition is to work as a Data Analyst or Business Intelligence Analyst 
+    in any industry where my expertise is required. I am very passionate about use
+      data to solve problems and increase business values and in the next 1-2 years, I 
+      want to acquire new knowledge, skills and work experience as well as
+       improving my existing ones. I am looking forward to becoming the Senior 
+       Data Analyst of a large organisation in the next 3-5 years 
+       so that I can use my knowledge and wealth
+         of experience to solve very challenging issues and have a significant influence on 
+         change that improves everyone's standard of living.
+    `,
+  },
   { que: `describe your leadership style`, ans: null },
-  { que: `do you have any questions for me?`, ans: null },
+  {
+    que: `do you have any questions for me?`,
+    ans: (
+      <>
+        <p>
+          Can you please tell me more about the day-to-day responsibilities of
+          the role?
+        </p>
+        <p>Can you please tell me more about the team I would be working in?</p>
+        <p>Can you please describe the working culture of the organisation?</p>
+        <p>
+          Are there opportunities for training and progression within the
+          role/company?
+        </p>
+      </>
+    ),
+  },
 ];
