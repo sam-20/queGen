d6c8b9dafa2b1818621ed66d93dce2289d47116c
Samuel Botwe <botwesamuel192@gmail.com>
2023-06-22 19:39:34 +0100

    update

--- src/pages/Questions/PowerBI Charts Practice.js
+++ src/pages/Questions/PowerBI Charts Practice.js
@@ -9,8 +9,8 @@ export const PBI = [
       <>
         <p>1 Categorical variable + 1 numerical variable</p>
         <p>Use Stacked Column Chart:</p>
-        <p>X-axis - categorical variable </p>
-        <p>Y-axis - numerical variable</p>
+        <p>Catg1 {"->"} X-axis</p>
+        <p>Num1 {"->"} Y-axis</p>
       </>
     ),
   },
@@ -20,18 +20,37 @@ export const PBI = [
       <>
         <p>2 or more Categorical variables + 1 Numeric variable</p>
         <p>Use stacked column chart: </p>
-        <p>X-axis, Legend - Categorical variables</p>
-        <p>Y-axis - numerical variable</p>
+        <p>Catg1 {"->"} X-axis</p>
+        <p>Catg2 {"->"} Legend</p>
+        <p>Num1 {"->"} Y-axis</p>
       </>
     ),
   },
   {
+    que: `add labels to bar/stacked bar chart`,
+    ans: `Visualizations pane -> Format Visual tab -> Data Labels`,
+  },
+  {
     que: `create a pie chart`,
-    ans: null,
+    ans: (
+      <>
+        <p>1 Categorical Variable + 1 numerical variable</p>
+        <p>Catg1 {"->"} Legend</p>
+        <p>Num1 {"->"} Values </p>
+      </>
+    ),
+  },
+  {
+    que: `update data labels of the pie chart slices ie. add or remove legend names, values`,
+    ans: `Visualizations pane -> Format Visual tab -> Detail Labels -> Label Contents`,
   },
   {
     que: `create a donut chart`,
     ans: null,
+  },
+  {
+    que: `view chart in a zoomed presentation view`,
+    ans: `click chart to make it active -> select Focus Mode in the top right `,
   },
   {
     que: `create a funnel chart`,
