6135a60eced523e10e954947bbeb96d07f2d0200
Samuel Botwe <botwesamuel192@gmail.com>
2023-06-24 20:47:43 +0100

    update

--- src/pages/Questions/PowerBI Dax Practice.js
+++ src/pages/Questions/PowerBI Dax Practice.js
@@ -1,6 +1,15 @@
 export const PDX = [
   {
-    que: ``,
-    ans: null,
+    que: `create a field or new measure`,
+    ans: (
+      <>
+        <p>
+          Home tab {"->"} Calculations group {"->"} New Measure
+        </p>
+        <p>eg. of measure</p>
+        <p>Measure_name = table[column_name]</p>
+        <p>Target Sales = SUM(Orders2[Sales])*1.3</p>
+      </>
+    ),
   },
 ];
